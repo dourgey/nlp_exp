33ae833b03853036eaca9cbe21d5efaabba40dba
zed <dourgey@gmail.com>
2021-10-31 20:05:30 +0800

    update bilstm attention

--- text_classification/src/models/RnnModel.py
+++ text_classification/src/models/RnnModel.py
@@ -9,6 +9,7 @@ class RnnConfig:
         self.n_embed = n_embed
         self.num_classes = num_classes
         self.embedding_pretrained = embedding_pretrained
+        self.use_attn = use_attn
 
         assert layer_type in ['RNN', 'LSTM', 'GRU']
         if layer_type == 'RNN':
@@ -26,6 +27,8 @@ class RnnConfig:
 class RnnModel(nn.Module):
     def __init__(self, config: RnnConfig):
         super(RnnModel, self).__init__()
+        self.use_attn = config.use_attn
+
         if config.embedding_pretrained is not None:
             self.embedding = nn.Embedding.from_pretrained(config.embedding_pretrained, freeze=False)
         else:
@@ -34,19 +37,30 @@ class RnnModel(nn.Module):
                                     num_layers=config.layer_num,
                                     bidirectional=config.bidirectional,
                                     batch_first=True, dropout=config.dropout)
-        if config.bidirectional:
-            self.fc = nn.Linear(config.hidden_size * 2, config.num_classes)
-        else:
-            self.fc = nn.Linear(config.hidden_size, config.num_classes)
+
+        D = 2 if config.bidirectional else 1
+        if self.use_attn:
+            self.attn_w = nn.Parameter(torch.randn(config.hidden_size * D, 1))
+            self.attn_b = nn.Parameter(torch.randn(config.hidden_size * D, ))
+        self.fc = nn.Linear(config.hidden_size * D, config.num_classes)
+
+    def attn(self, h_t):
+        h_t = torch.tanh(h_t)
+        alpha = torch.softmax(torch.matmul(h_t, self.attn_w) + self.attn_b, dim=-1)
+        out = h_t * alpha
+        return out
 
     def forward(self, x):
         x = self.embedding(x)
-        x = self.rnn(x)[0]
-        out = self.fc(x[:, -1])
-        return out
+        x, _ = self.rnn(x)
+        if self.use_attn:
+            x = torch.sum(self.attn(x), dim=1)
+            return self.fc(x)
+        else:
+            return self.fc(x[:, -1])
 
 if __name__ == '__main__':
-    config = RnnConfig(100, 128, 8, None, layer_num=2, bidirectional=True)
+    config = RnnConfig(100, 128, 8, None, layer_num=2, bidirectional=True, use_attn=True)
     rnn = RnnModel(config)
     x = torch.tensor([[1, 3], [2, 6]])
     print(rnn(x))
